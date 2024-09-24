fb1061aa4055490bd44d07d09b707bb94c655ba9
AdsonTanajura <adson.tanajura@gmail.com>
2024-09-24 14:24:05 -0300

    Convert Celsius para Fahrenheit

---
+++ main.js
@@ -0,0 +1,5 @@
+const celsius = parseFloat(prompt('Digite a temperatura em celsisus: '));
+
+const fahrenheit = celsius * 1.8 + 32;
+
+alert('A temperatura em Fahrenheit é de: ' + fahrenheit.toFixed(2));
