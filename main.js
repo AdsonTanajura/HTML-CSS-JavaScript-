bad92fc19c3c64aa41712d118210bab107dbde16
AdsonTanajura <adson.tanajura@gmail.com>
2024-09-25 15:53:31 -0300

    Fiötrando lista

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
-const facebookButton = document.getElementById('facebook-button');
-const twitterButton = document.getElementById('twitter-button');
+const search = document.querySelector('#search');
+const list = document.querySelector('#list');
 
-function shareOnFacebook() {
-  const url = encodeURIComponent(window.location.href);
-  const shareUrl = `https://www.facebook.com/sharer/sharer.php?u=${url}`;
-  window.open(shareUrl, '_blank');
-}
-
-function shareOnTwitter() {
-  const text = encodeURIComponent('Check out this website:');
-  const url = encodeURIComponent(window.location.href);
-  const shareUrl = `https://twitter.com/intent/tweet?text=${text}&url=${url}`;
-  window.open(shareUrl, '_blank');
-}
-
-facebookButton.addEventListener('click', shareOnFacebook);
-twitterButton.addEventListener('click', shareOnTwitter);
+search.addEventListener('input', () => {
+  const value = search.value.toLowerCase();
+  const items = list.getElementsByTagName('li');
+  Array.from(items).forEach((item) => {
+    const text = item.textContent.toLowerCase();
+    if (text.indexOf(value) !== -1) {
+      item.style.display = 'block';
+    } else {
+      item.style.display = 'none';
+    }
+  });
+});
