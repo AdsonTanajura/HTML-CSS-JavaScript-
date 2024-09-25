8a9b40e9349b51f609183678e2ba6f418d7137bd
AdsonTanajura <adson.tanajura@gmail.com>
2024-09-24 23:57:00 -0300

    Carregando e alterando o tamnho da imagens

--- main.js
+++ main.js
@@ -1,19 +1,1 @@
-const numberInput = document.getElementById("numberInput");
-const checkButton = document.getElementById("checkButton");
-const outputDiv = document.getElementById("output");
 
-checkButton.addEventListener("click", function() {
-  const number = Number(numberInput.value);
-
-  if (number >= 1 && number <= 10) {
-    outputDiv.innerHTML = "Valid number!";
-  } else if (number < 1) {
-    outputDiv.innerHTML = "Number is too small!";
-  } else if (number > 10) {
-    outputDiv.innerHTML = "Number is too large!";
-  } else {
-    outputDiv.innerHTML = "Something went wrong!";
-  }
-
-  numberInput.value = ""
-});
