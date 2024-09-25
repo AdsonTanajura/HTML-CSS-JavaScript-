ca3e2870d154d6f4ea54f6c8624cbcdcfd426d7a
AdsonTanajura <adson.tanajura@gmail.com>
2024-09-24 22:12:57 -0300

    Adivinhe o numero

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
-const gretting = (name) => {
-    console.log('Hi ' + name )
+const randomNumber = Math.floor(Math.random() * 100);
+let tries = 0;
+
+
+const checkGuess = () => {
+    const guess = document.getElementById('guess').value;
+    const message = document.getElementById('message');
+    tries = tries + 1;
+
+
+    if (guess == randomNumber) {
+        message.innerHTML = 'Congratulations, you guessed the number in ' + tries + '!';
+    }
+    else if (guess < randomNumber) {
+        message.innerHTML = 'Too low. Try again!';
+    }
+    else {
+        message.innerHTML = 'Too high. Try again!';
+    };
+
 };
-
-gretting('Adson');
-
-const sum = (n1, n2) => {
-    return n1 + n2;
-};
-
-console.log(sum(20, 10));
